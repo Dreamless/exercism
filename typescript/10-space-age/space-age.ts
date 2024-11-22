dd9e586ffaaa4219bb29edb4305343357debb8b9
Dreamless <selected.dreams@gmail.com>
2024-11-22 06:49:26 +0500

    TS-10 Space age

---
+++ typescript/10-space-age/space-age.ts
@@ -0,0 +1,23 @@
+type Planet = 'mercury' | 'venus' | 'earth' | 'mars' | 'jupiter' | 'saturn' | 'uranus' | 'neptune';
+type PlanetOrbitalPeriods = Record<Planet, number>;
+
+export function age(planet: Planet, seconds: number): number {
+	const EARTH_YEAR_SECONDS = 31557600;
+
+	const ORBITAL_PERIODS: PlanetOrbitalPeriods = {
+		mercury: 0.2408467,
+		venus: 0.61519726,
+		earth: 1.0,
+		mars: 1.8808158,
+		jupiter: 11.862615,
+		saturn: 29.447498,
+		uranus: 84.016846,
+		neptune: 164.79132
+	};
+
+	const orbitalPeriod = ORBITAL_PERIODS[planet];
+	const earthYears = seconds / EARTH_YEAR_SECONDS;
+	const planetAge = earthYears / orbitalPeriod;
+
+	return parseFloat(planetAge.toFixed(2));
+}
