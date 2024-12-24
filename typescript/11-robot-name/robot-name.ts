f31b8a0f44b7e84196335dafdd7229480f579592
Dreamless <selected.dreams@gmail.com>
2024-12-25 00:41:04 +0500

    TS-11 Robot name (draft 2)

--- typescript/11-robot-name/robot-name.ts
+++ typescript/11-robot-name/robot-name.ts
@@ -1,54 +1,57 @@
 export class Robot {
-  private static usedNames: Set<string> = new Set();
   private _name: string | null = null;
-  private static serialNumber = 0;
+  private static allNames: string[] = [];
 
-  constructor() {}
+  constructor() {
+    Robot.allNames = Robot.generateAllNames();
+  }
 
   private static generateAllNames(): string[] {
-    let allNames: string[] = [];
-    for (let i = 0; i < 677; i++) {
-      const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const allNames: string[] = [];
+
+    for (let i = 0; i < 676; i++) {
       const firstLetter = letters[Math.floor(i / letters.length)];
       const secondLetter = letters[i % letters.length];
-      const names: string[]  = Array.from({ length: 1000 }, (_, index) => {
-        return firstLetter + secondLetter + index;
-      });
-      allNames = allNames.concat(names);
+
+      for (let numIndex = 0; numIndex < 1000; numIndex++) {
+        allNames.push(`${firstLetter}${secondLetter}${numIndex.toString().padStart(3, '0')}`);
+      }
     }
+
     return allNames;
   }
 
-  private static generateUniqueName(): string {
-    this.generateAllNames();
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const firstLetter = letters[Math.floor(this.serialNumber / letters.length)];
-    const secondLetter = letters[this.serialNumber % letters.length];
-    const generateSerialNumber = this.serialNumber.toString().padStart(3, '0');
-
-
-    let name = firstLetter + secondLetter + generateSerialNumber;
-    // const generateRandomName = (qty: number): string => {
-    //   //const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    //   let str = "";
-    //   while (str.length < qty) {
-    //     str += letters[Math.floor(Math.random() * letters.length)];
-    //   }
-    //   return str;
-    // }
-    //
-    // do {
-    //   name = firstLetter + secondLetter + generateSerialNumber;
-    // } while (this.usedNames.has(name));
-
-    this.usedNames.add(name);
-    this.serialNumber++;
-    return name;
-  }
+  // private static generateUniqueName(): string {
+  //   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //   const firstLetter = letters[Math.floor(this.serialNumber / letters.length)];
+  //   const secondLetter = letters[this.serialNumber % letters.length];
+  //   const generateSerialNumber = this.serialNumber.toString().padStart(3, '0');
+  //
+  //
+  //   let name = this.allNames;
+  //   // const generateRandomName = (qty: number): string => {
+  //   //   //const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //   //   let str = "";
+  //   //   while (str.length < qty) {
+  //   //     str += letters[Math.floor(Math.random() * letters.length)];
+  //   //   }
+  //   //   return str;
+  //   // }
+  //   //
+  //   // do {
+  //   //   name = firstLetter + secondLetter + generateSerialNumber;
+  //   // } while (this.usedNames.has(name));
+  //
+  //   this.usedNames.add(name);
+  //   this.serialNumber++;
+  //   return name;
+  // }
 
   public get name(): string {
     if (!this._name) {
-      this._name = Robot.generateUniqueName();
+      const randomNameIndex = Math.floor(Math.random() * Robot.allNames.length);
+      this._name = Robot.allNames[randomNameIndex];
     }
     return this._name;
   }
@@ -58,6 +61,6 @@ export class Robot {
   }
 
   public static releaseNames(): void {
-    this.usedNames.clear();
+    this.allNames = [];
   }
 }
