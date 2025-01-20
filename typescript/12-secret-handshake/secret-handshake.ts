312f7ffebdab7a87f1e72b5cf683867b25b6b4f5
Dreamless <selected.dreams@gmail.com>
2025-01-20 18:31:06 +0500

    TS-12 Reworked without string manipulations (approved version)

--- typescript/12-secret-handshake/secret-handshake.ts
+++ typescript/12-secret-handshake/secret-handshake.ts
@@ -3,7 +3,7 @@ export function commands(number: number): string[] {
   const result: string[] = [];
 
   for (let i = 0; i < actions.length; i++) {
-    if (number % 2 === 1 && number > 0) {
+    if (number % 2 === 1) {
       result.push(actions[i])
     }
     number = Math.floor(number / 2);
