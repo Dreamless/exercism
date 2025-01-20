a5ff46481423a3e812476280f65c5d38924f14d0
Dreamless <selected.dreams@gmail.com>
2025-01-20 17:17:05 +0500

    TS-12 Reworked without string manipulations

--- typescript/12-secret-handshake/secret-handshake.ts
+++ typescript/12-secret-handshake/secret-handshake.ts
@@ -1,16 +1,16 @@
 export function commands(number: number): string[] {
   const actions = ['wink', 'double blink', 'close your eyes', 'jump'];
-  const bin = number.toString(2).padStart(5, '0');
   const result: string[] = [];
 
-  for (let i = 0; i < 4; i++) {
-    if (bin[4 - i] === '1') {
-      result.push(actions[i]);
+  for (let i = 0; i < actions.length; i++) {
+    if (number % 2 === 1 && number > 0) {
+      result.push(actions[i])
     }
+    number = Math.floor(number / 2);
   }
 
-  if (bin[0] === '1') {
-    result.reverse();
+  if (number === 1) {
+    return result.reverse();
   }
 
   return result;
